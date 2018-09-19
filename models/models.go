59aacb0ab33fb9a0404f05c54cd0526b41313fe2
Qingcai <qingcai518@gmail.com>
2018-09-19 17:09:45 +0900

    change mysql

--- models/models.go
+++ models/models.go
@@ -7,7 +7,8 @@ import (
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "root:BCTech_8888@/digital_receipt?charset=utf8")
+	//orm.RegisterDataBase("default", "mysql", "root:BCTech_8888@/digital_receipt?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "root:longhash@/digital_receipt?charset=utf8")
 	orm.SetMaxIdleConns("default", 100)
 	orm.SetMaxOpenConns("default", 100)
 
